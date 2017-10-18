fa7d1523df02dea77bb8cd653eb74c3d65941c02
Benedikt Spranger <b.spranger@linutronix.de>
2017-10-18 09:25:39 +0200

    enhance errorhandling in cyclictest
    Due to the underlaying structure of jenkins pipeline jobs exceptions may
    loose some meaningfull error informations.
    Catch exceptions, report informations about the exception and reraise the
    exception to inform Jenkins about the error.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>

--- vars/cyclictest.groovy
+++ vars/cyclictest.groovy
@@ -5,24 +5,36 @@
 
 package de.linutronix.cirt;
 
+import de.linutronix.cirt.inputcheck;
+
 def call(Map global, String target, String[] cyclictests) {
-	node('master') {
-		for (int i = 0; i < cyclictests.size(); i++) {
-			ct = cyclictests[i];
+	try {
+                inputcheck.check(global);
+		node('master') {
+			for (int i = 0; i < cyclictests.size(); i++) {
+				ct = cyclictests[i];
 
-			String[] properties = ["environment.properties",
-					       "${ct}.properties"];
-			helper = new helper();
-			deleteDir();
-			unstash(global.STASH_PRODENV);
-			helper.add2environment(properties);
+				String[] properties = ["environment.properties",
+						       "${ct}.properties"];
+				helper = new helper();
+				deleteDir();
+				unstash(global.STASH_PRODENV);
+				helper.add2environment(properties);
 
-			interval = helper.getEnv("INTERVAL");
-			limit = helper.getEnv("LIMIT");
-			loadgen = helper.getEnv("LOADGEN");
-			cyclictestRunner(target, ct, interval, limit, loadgen);
+				interval = helper.getEnv("INTERVAL");
+				limit = helper.getEnv("LIMIT");
+				loadgen = helper.getEnv("LOADGEN");
+				cyclictestRunner(target, ct, interval, limit,
+						 loadgen);
+			}
 		}
-	}
+	} catch(Exception ex) {
+                println("cyclictest on ${target} failed:");
+                println(ex.toString());
+                println(ex.getMessage());
+                println(ex.getStackTrace());
+                error("cyclictest on ${target} failed.");
+        }
 }
 
 def call(String... params) {
