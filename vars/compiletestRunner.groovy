da2d9be6d871fec51934edaaff9f13fd2ef56bcd
Anna-Maria Gleixner <anna-maria@linutronix.de>
2017-11-30 10:00:01 +0100

    vars/compiletestRunner: Integrate compile script in groovy
    The compile script, that creates the compile command script, is stored
    as an external resource bash script. The external resource bash script
    is executed by using the helper class. The helper class injects
    several environment settings before executing the script. This makes
    the code harder to read and harder to debug.
    
    Integrate the creation of the compile bash script in groovy. Add the
    required environment variables to the script.
    
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- vars/compiletestRunner.groovy
+++ vars/compiletestRunner.groovy
@@ -52,7 +52,88 @@ private runner(Map global, String repo, String branch,
 
 			add2environment(properties);
 			runShellScript("compiletest/preparekernel.sh");
-			runShellScript("compiletest/compile.sh");
+
+			/*
+			 * Use environment file and add an "export "
+			 * at the begin of every line, to be
+			 * includable in compile bash script; remove
+			 * empty lines before adding "export ".
+			 */
+			def exports = readFile ".env/compile/env/${arch}.properties";
+			exports = exports.replaceAll(/(?m)^\s*\n/, "");
+			exports = exports.replaceAll(/(?m)^/, "export ");
+
+			def script_content = """#!/bin/bash
+
+# Exit bash script on error:
+set -e
+
+# Required environment settings
+${exports}
+export ARCH=${arch}
+
+SCHEDULER_ID=${getEnv("SCHEDULER_ID")}
+CONFIGNAME=${configFile.getName()}
+overlay=${overlay}
+BUILD=build
+RESULT=compile
+
+"""+'''
+
+# Make devicetree binaries?
+if [ x = x"$MKDTBS" ]
+then
+	export MKDTBSCMD=""
+else
+	export MKDTBSCMD="make -j ${PARALLEL_MAKE_JOBS:=16} O=build LOCALVERSION=-$SCHEDULER_ID dtbs"
+fi
+
+
+# Build only and do not boot config? packaging of kernel is not required
+
+if [ ! -f .env/compile/env/$ARCH-$CONFIGNAME-$overlay.properties ]
+then
+	BUILDONLY=1
+else
+	BUILDONLY=0
+fi
+
+# Create build/cmd script to build/package the kernel and dtbs
+cat << _EOF_ > $BUILD/cmd
+# Abort build script if there was an error executing the commands
+set -e
+
+echo "compiletest-runner #$BUILD_NUMBER $ARCH/$CONFIGNAME (stderr)" > $RESULT/compile.log
+make -j ${PARALLEL_MAKE_JOBS:=16} O=$BUILD LOCALVERSION=-$SCHEDULER_ID 2> >(tee -a $RESULT/compile.log >&2)
+$MKDTBSCMD
+
+_EOF_
+
+
+# If config will be booted later, debian package and devicetrees
+# need to be created and stored in $RESULT
+if [ $BUILDONLY -eq 0 ]
+then
+	cat << _EOF_ >> $BUILD/cmd
+make -j ${PARALLEL_MAKE_JOBS:=16} O=$BUILD LOCALVERSION=-$SCHEDULER_ID ${BUILD_TARGET:-bindeb-pkg} 2> >(tee $RESULT/package.log >&2)
+
+if [ -d $BUILD/arch/${ARCH}/boot/dts ]
+then
+	tar cJf $RESULT/dtbs-${SCHEDULER_ID}.tar.xz --exclude 'dts/.*' -C $BUILD/arch/${ARCH}/boot dts
+fi
+cp *deb $RESULT/
+_EOF_
+
+fi
+
+chmod a+x $BUILD/cmd
+cp $BUILD/cmd $RESULT/
+
+# Execute Buildcmd
+$BUILD/cmd''';
+
+			writeFile file:"${getEnv("RESULT")}/compile-script.sh", text:script_content;
+			sh ". ${getEnv("RESULT")}/compile-script.sh";
 
 			def linuximage = "${config}/${overlay}";
 			stash(name: linuximage.replaceAll('/','_'),
