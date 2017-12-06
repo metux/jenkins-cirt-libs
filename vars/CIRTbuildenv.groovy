b6f5fa703e537112f564cc516d6bf86a8d7ee287
Benedikt Spranger <b.spranger@linutronix.de>
2017-12-06 12:05:37 +0100

    vars/CIRTbuildenv: Integrate external script compile-boot.sh
    The external shell script compile-boot.sh determine boottest related
    environment variables. For a better maintainance and a straighter test
    structure external scripts and resources are deprecated.
    
    Integrate external script compile-boot.sh into CIRTbuildenv.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

--- vars/CIRTbuildenv.groovy
+++ vars/CIRTbuildenv.groovy
@@ -185,17 +185,55 @@ private buildCyclictestEnv(List boottests) {
 }
 
 private prepareCompBootEnv(String config, String overlay) {
-        helper = new helper();
-        helper.extraEnv("config", config);
-        helper.extraEnv("overlay", overlay);
-        helper.runShellScript("environment/compile-boot.sh");
+	sh """\
+#! /bin/bash
+
+set -e
+
+export config=${config}
+export overlay=${overlay}
+""" + '''\
+ARCH=$(dirname $config)
+CONFIGNAME=$(basename $config)
+
+BOOTTESTS=" "
+
+echo "building compile/env/$ARCH-$CONFIGNAME-$overlay.properties"
+
+# Read env/boottest.list line by line
+while read line
+do
+    case $line in
+        # ignore comments
+        "#"*)
+        ;&
+        "")
+            continue
+            ;;
+        # find $config and $overlay in all other boottest files
+        *)
+            if grep -q -G "^CONFIG[ ]*=[ ]*${config}$" $line && grep -q -G "^OVERLAY[ ]*=[ ]*${overlay}$" $line
+            then
+                BOOTTESTS="$BOOTTESTS $line"
+            fi
+            ;;
+    esac
+done < env/boottest.list
+
+# create compile/env/$ARCH-$NAME-$OVERLAY.properties file
+if [ ! -z $BOOTTESTS ]
+then
+    echo "BOOTTESTS=$BOOTTESTS" > compile/env/$ARCH-$CONFIGNAME-$overlay.properties
+else
+    echo "No boottest configured: Property file creation skipped."
+fi
+'''
 }
 
-private buildCompBootEnv(String[] configs, String[] overlays) {
-        for (int i = 0; i < configs.size(); i++) {
-                for (int j = 0; j < overlays.size(); j++) {
-                        prepareCompBootEnv(configs.getAt(i),
-                                   overlays.getAt(j));
+private buildCompBootEnv(List configs, List overlays) {
+        configs.each { config ->
+                overlays.each { overlay ->
+                        prepareCompBootEnv(config, overlay);
                 }
         }
 }
@@ -212,7 +250,7 @@ private buildCompileEnv() {
 	buildArchCompileEnv(configs);
 
 	if (boottests) {
-		buildCompBootEnv(configs as String[], overlays as String[]);
+		buildCompBootEnv(configs, overlays);
 		buildCyclictestEnv(boottests);
 	}
 }
