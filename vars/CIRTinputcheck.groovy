5d3407bd83f8b1d0b9a0dbb15e367972709b6a03
Anna-Maria Gleixner <anna-maria@linutronix.de>
2017-12-07 16:32:11 +0100

    vars/CIRTinputcheck: Add a dedicated groovy class for inputcheck
    Inputcheck examines, if all parameters are set properly. It is done
    via a groovy source file. If a Jenkinsfile should directly call
    inputcheck, outsourcing it into a dedicated class eases the
    call. Otherwise the path needs to be imported.
    
    The test itself defines on which node it has to run. Therefore add the
    node{} closure. Furthermore GUI_TESTDESCR_BRANCH is added to the
    handed in map, so only a single call() method is required.
    
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Reviewed-by: Manuel Traut <manut@linutronix.de>

---
+++ vars/CIRTinputcheck.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/groovy
+/* -*- mode: groovy; -*-
+ * CI-RT common input checks
+ */
+
+package de.linutronix.cirt;
+
+def call(Map global) {
+	node ('master') {
+		if (!global.STASH_PATCHES?.trim()) {
+			error("variable STASH_PATCHES not set.");
+		}
+
+		if (!global.STASH_PRODENV?.trim()) {
+			error("variable STASH_PRODENV not set.");
+		}
+
+		if (!global.STASH_RAWENV?.trim()) {
+			error("variable STASH_RAWENV not set.");
+		}
+
+		if (!global.STASH_COMPILECONF?.trim()) {
+			error("variable STASH_COMPILECONF not set.");
+		}
+
+		if (!global.TESTDESCRIPTION_REPO?.trim()) {
+			error("variable TESTDESCRIPTION_REPO not set.");
+		}
+
+		if (!global.GUI_TESTDESCR_BRANCH?.trim()) {
+			error("variable GUI_TESTDESCR_BRANCH not set.");
+		}
+	}
+}
+
+def call(String... params) {
+	println params
+	error("Unknown signature. Abort.");
+}
