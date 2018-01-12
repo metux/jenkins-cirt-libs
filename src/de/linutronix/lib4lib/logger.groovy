52cc8d62363f2e24e26335ca2d700fb536f6888f
Benedikt Spranger <b.spranger@linutronix.de>
2018-01-13 00:48:50 +0100

    Add logging helper
    Jenkins uses java.util.loging to log messages into a system log.
    Messages are taged by a group name and log level. Add log functions
    to be used in lib4lib.
    The messages are taged with the group name "lib4lib" and the log level
    characterized by function:
    debugMsg: Level.FINE
    infoMsg : Level.INFO
    warnMsg : Level.WARNING
    errorMsg: Level.SEVERE
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Manuel Traut <manut@linutronix.de>
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Reviewed-by: Manuel Traut <manut@linutronix.de>

---
+++ src/de/linutronix/lib4lib/logger.groovy
@@ -0,0 +1,41 @@
+#!/usr/bin/groovy
+/* -*- mode: groovy; -*-
+ * helpers to ensure a debian package is installed in the correct version
+ */
+
+package de.linutronix.lib4lib;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * debugMsg - Log a message to Jenkins Log (Level.FINE)
+ * @param msg	log message
+ */
+static debugMsg(String msg) {
+	Logger.getLogger("lib4lib").log(Level.FINE, msg);
+}
+
+/**
+ * infoMsg - Log a message to Jenkins Log (Level.INFO)
+ * @param msg	log message
+ */
+static infoMsg(String msg) {
+	Logger.getLogger("lib4lib").log(Level.INFO, msg);
+}
+
+/**
+ * warnMsg - Log a message to Jenkins Log (Level.WARNING)
+ * @param msg	log message
+ */
+static warnMsg(String msg) {
+	Logger.getLogger("lib4lib").log(Level.WARNING, msg);
+}
+
+/**
+ * errorMsg - Log a message to Jenkins Log (Level.SEVERE)
+ * @param msg	log message
+ */
+static errorMsg(String msg) {
+	Logger.getLogger("lib4lib").log(Level.SEVERE, msg);
+}
