645fd148e4826a82f12cb31dce5ba286e194b884
Benedikt Spranger <b.spranger@linutronix.de>
2018-01-13 00:48:52 +0100

    Add shunit command
    To perform tests and create JUnit result files pyjutest is used. pyjutest
    creates a junit xml based on stdout and stderr of a passed command.
    
    This implements a wrapper function that ensures that a given command is
    passed to run with pyjutest wrapped.
    
    ensureDebPkg is used to ensure that pyjutest is installed on the targetnode.
    ensureDebPkg is not able to support different suites on the targetnodes
    atm.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Manuel Traut <manut@linutronix.de>
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Reviewed-by: Manuel Traut <manut@linutronix.de>

---
+++ vars/shunit.groovy
@@ -0,0 +1,49 @@
+#!/usr/bin/groovy
+/* -*- mode: groovy; -*-
+ * CI-RT library kernel build test
+ */
+
+package de.linutronix.lib4lib;
+
+import de.linutronix.lib4lib.logger;
+
+def call(String classname, String name, String cmd) {
+
+	Map enspkg = [:]
+	enspkg.ensure_pkgs = [[name: 'pyjutest', version: '1.6+stretch251']];
+	// TODO skip suite name here and set it in ensureDebPkg
+	enspkg.ensure_repo = "deb http://debian.linutronix.de/tools stretch main"
+	enspkg.ensure_repo_key = "http://debian.linutronix.de/tools/repo.pub"
+	ensureDebPkg(enspkg);
+
+	writeFile(file:"pyjutest.sh", text:cmd);
+	sh "chmod a+x ./pyjutest.sh"
+
+	testcmd = "pyjutest";
+	if (classname) {
+		testcmd += " --class \"${classname}\"";
+	}
+	if (name) {
+		testcmd += " --name \"${name}\"";
+	}
+	testcmd += " ./pyjutest.sh";
+	logger.debugMsg("--- Start Test ---");
+	logger.debugMsg(cmd);
+	logger.debugMsg("run: " + testcmd);
+	sh(testcmd);
+	logger.debugMsg("--- End Test ---");
+	junit("pyjutest.xml");
+}
+
+def call(String classname, String cmd) {
+	call(classname, null, cmd);
+}
+
+def call(String cmd) {
+	call(null, null, cmd);
+}
+
+def call(String... params) {
+	println params
+	error("Unknown signature. Abort.");
+}
