2bc5c4b60fe2d868abb1824799af065424ce8320
Anna-Maria Gleixner <anna-maria@linutronix.de>
2018-01-15 11:25:54 +0100

    helper: Remove no longer used runShellScript() calls
    All shell scripts are removed which required a dedicated environment
    setting. Therefore runShellScript() and related loadShellScript()
    function can be removed.
    
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Reviewed-by: Manuel Traut <manut@linutronix.de>

--- src/de/linutronix/cirt/helper.groovy
+++ src/de/linutronix/cirt/helper.groovy
@@ -9,38 +9,6 @@ import groovy.transform.Field
 @Field String prefix = "de/linutronix/cirt/";
 @Field Map environment = [:];
 
-def String loadShellScript(String name) {
-	script = libraryResource(prefix + name);
-
-	return script;
-}
-
-def runShellScript(String name) {
-	runShellScript(name, " ");
-}
-
-/*
- * runShellScript is required for reproducability of CI-RT without Jenkins:
- * a) use scripts with proper values outside of ci-rt environment
- * b) save scripts in db along with test results (not implemented yet, TODO)
- */
-def runShellScript(String name, String args) {
-	script = loadShellScript(name);
-	println "Running shell script \"${name}\""
-	try {
-		withEnv(environment.collect { k,v -> return "${k}=${v}" }) {
-			sh "${script} ${args}";
-		}
-	} catch(Exception ex) {
-		println("shell script \"${name}\" failed:");
-		println(ex.toString());
-		println(ex.getMessage());
-		println(ex.getStackTrace());
-		error("shell script \"${name}\" failed.");
-	}
-	println "Finish shell script \"${name}\""
-}
-
 def extraEnv(String k, String v) {
 	environment[k] = v;
 }
