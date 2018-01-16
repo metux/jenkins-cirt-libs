2bac6d9f43dabb18924b5eb5c79f7df7c42e065d
Manuel Traut <manut@linutronix.de>
2018-01-16 15:33:29 +0100

    resources/... : Delete unused resources
    There is no usage of these resources in lib-cirt or in any of the
    pipelines. Artefacts were created during the pipeline rework.
    
    Signed-off-by: Manuel Traut <manut@linutronix.de>
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

--- resources/de/linutronix/cirt/boottest/markonline.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# Exit bash script on error:
-set -e
-
-# Update boottest notes: add "Target temporary not available"
-# $PGSQL -c "UPDATE boottest SET notes = "\'"Target temporary not available"\'";" RT-Test
-echo "[99999.999999] Target $TARGET temporary not available" > result/boot.log
