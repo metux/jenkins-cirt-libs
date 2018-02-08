757e8649ef21b25ebbfcf9ceea3aee41fc75af98
Benedikt Spranger <b.spranger@linutronix.de>
2018-02-08 12:26:39 +0100

    cyclictest: Reorg result parsing
    Split out result parsing into a function for better readability.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

--- vars/cyclictest.groovy
+++ vars/cyclictest.groovy
@@ -6,6 +6,36 @@
 import de.linutronix.cirt.inputcheck;
 import de.linutronix.cirt.helper;
 
+private parse_results(Map global, String target, String ct)
+{
+	unstash(global.STASH_PRODENV);
+	h = new helper();
+	String[] properties = ["environment.properties",
+			       "boot/${target}.properties",
+			       "${ct}.properties"];
+	h.add2environment(properties);
+	config = h.getEnv("CONFIG");
+	overlay = h.getEnv("OVERLAY");
+	kernel = "${config}/${overlay}";
+
+	cyclictestdir = "results/${kernel}/${target}/${ct}";
+	unstash(ct.replaceAll('/','_'));
+
+	script_content = libraryResource('de/linutronix/cirt/cyclictest/cyclictest2xml.py');
+	writeFile file:"collect", text:script_content;
+	sh("python3 collect ${ct} ${cyclictestdir} \
+	    --entryowner ${h.getEnv('ENTRYOWNER')}			\
+	    --duration ${h.getEnv('DURATION')}				\
+	    --interval ${h.getEnv('INTERVAL')}				\
+	    --limit ${h.getEnv('LIMIT')}")
+
+	junit("${cyclictestdir}/pyjutest.xml");
+	archiveArtifacts("${cyclictestdir}/pyjutest.xml");
+	stash(name: ct.replaceAll('/','_'),
+	      includes: "${cyclictestdir}/histogram.*," +
+			"${cyclictestdir}/pyjutest.xml");
+}
+
 def call(Map global, String target, String[] cyclictests) {
 	try {
                 inputcheck.check(global);
@@ -20,33 +50,7 @@ def call(Map global, String target, String[] cyclictests) {
 					 * workspace directory doesn't has to be changed
 					 */
 					cyclictestRunner(global, target, ct);
-
-					unstash(global.STASH_PRODENV);
-					h = new helper();
-					String[] properties = ["environment.properties",
-						"boot/${target}.properties",
-						"${ct}.properties"];
-					h.add2environment(properties);
-					config = h.getEnv("CONFIG");
-					overlay = h.getEnv("OVERLAY");
-					kernel = "${config}/${overlay}";
-
-					cyclictestdir = "results/${kernel}/${target}/${ct}";
-					unstash(ct.replaceAll('/','_'));
-
-					script_content = libraryResource('de/linutronix/cirt/cyclictest/cyclictest2xml.py');
-					writeFile file:"collect", text:script_content;
-					sh("python3 collect ${ct} ${cyclictestdir}\
-						--entryowner ${h.getEnv('ENTRYOWNER')}\
-						--duration ${h.getEnv('DURATION')}\
-						--interval ${h.getEnv('INTERVAL')}\
-						--limit ${h.getEnv('LIMIT')}")
-
-					junit("${cyclictestdir}/pyjutest.xml");
-					archiveArtifacts("${cyclictestdir}/pyjutest.xml");
-					stash(name: ct.replaceAll('/','_'),
-					      includes: "${cyclictestdir}/histogram.*," + \
-							"${cyclictestdir}/pyjutest.xml");
+					parse_results(global, target, ct);
 				}
 			}
 		}
