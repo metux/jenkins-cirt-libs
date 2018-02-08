6bc91e2ae82f9079bdb953f92bacf94c3ab72e06
Benedikt Spranger <b.spranger@linutronix.de>
2018-02-08 12:26:44 +0100

    Add email notification
    Add a global notification variable for a convinient way to inform
    about errors and warnings.
    
    [anna-maria: fix whitespace damage]
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

---
+++ vars/notify.groovy
@@ -0,0 +1,58 @@
+#!/usr/bin/groovy
+/* -*- mode: groovy; -*-
+ * notification helper
+ */
+
+import groovy.text.StreamingTemplateEngine
+import java.net.URI
+
+def renderTemplate(String input, Map variables)
+{
+	def engine = new StreamingTemplateEngine()
+	return engine.createTemplate(input).make(variables).toString()
+}
+
+def call(String to, String subject, String template, Map variables)
+{
+	call(to, subject, template, null, true, variables);
+}
+
+def call(String to, String subject)
+{
+	call(to, subject, "default", null, true, [:]);
+}
+
+def call(String to, String subject, String templatename, String attachment,
+	 Boolean log, Map variables)
+{
+	if (to == null) {
+		error("No E-Mail adress given. Abort.");
+	}
+
+	if (templatename == null) {
+		error("No E-Mail template given. Abort.");
+	}
+
+	println("Send Email notification to ${to}");
+	println("Email template: ${templatename}");
+
+	up = upstreaminfo();
+
+	variables << currentBuild.getRawBuild().getEnvironment();
+	variables << up;
+
+	def template = libraryResource("de/linutronix/cirt/email/${templatename}.txt");
+	def body = renderTemplate(template, variables);
+
+	URI uri = new URI(Hudson.instance.getRootUrl());
+	subject = "[${uri.getHost().replaceAll(/\..*/, "")}] ${subject}";
+
+	emailext(subject: subject, body: body, attachLog: log,
+		 attachmentsPattern: attachment, mimeType: "text/plain",
+		 to: to);
+}
+
+def call(String... params) {
+	println params
+	error("Unknown signature. Abort.");
+}
