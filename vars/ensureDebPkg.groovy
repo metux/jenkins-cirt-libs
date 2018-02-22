3169dff0e71fc1033bded0035859a7480aa54baf
Anna-Maria Gleixner <anna-maria@linutronix.de>
2018-02-22 11:20:10 +0100

    vars/ensureDebPkg: Add missing semicolons at end of line
    The current convention of coding style is, that at the end of each
    line a semicolon should be added. ensureDebPkg doesn't match this
    convention.
    
    This is a style fix only and no functional change is done.
    
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- vars/ensureDebPkg.groovy
+++ vars/ensureDebPkg.groovy
@@ -8,14 +8,14 @@ import de.linutronix.lib4lib.logger;
 def check_pkgversion(String pkg, String dver)
 {
 	// raises an exception if pkg is not available
-	sh("dpkg -l $pkg | grep ^ii")
+	sh("dpkg -l $pkg | grep ^ii");
 	if (dver != "None") {
 		// used to check version
-		def dpkg = sh(returnStdout: true, script: "dpkg -l $pkg | grep ^ii")
-		def dpkg_pkg = dpkg.split()[1]
-		def dpkg_ver = dpkg.split()[2]
+		def dpkg = sh(returnStdout: true, script: "dpkg -l $pkg | grep ^ii");
+		def dpkg_pkg = dpkg.split()[1];
+		def dpkg_ver = dpkg.split()[2];
 		// this raises an exception if the package version is not correct
-		sh("dpkg --compare-versions $dpkg_ver = $dver")
+		sh("dpkg --compare-versions $dpkg_ver = $dver");
 	}
 }
 
@@ -35,27 +35,27 @@ def call(Map p = [:])
 		def pkg = p.ensure_pkgs[i].name;
 		def dver = "None";
 		if (p.ensure_pkgs[i].version)
-			dver = p.ensure_pkgs[i].version
+			dver = p.ensure_pkgs[i].version;
 		try {
-			check_pkgversion(pkg, dver)
+			check_pkgversion(pkg, dver);
 		} catch (err) {
 			// ensure reinstall and dont fail if not installed
-			sh("sudo apt-get --yes purge $pkg | /bin/true")
+			sh("sudo apt-get --yes purge $pkg | /bin/true");
 			// TODO guess suite name if not specified
-			sh("echo \'$p.ensure_repo\' | sudo tee /etc/apt/sources.list.d/ensure.list")
+			sh("echo \'$p.ensure_repo\' | sudo tee /etc/apt/sources.list.d/ensure.list");
 			if (dver != "None") {
-				sh("echo \'Package: $pkg\nPin: version $dver\nPin-Priority: 1001\' | sudo tee /etc/apt/preferences.d/ensure.pref")
+				sh("echo \'Package: $pkg\nPin: version $dver\nPin-Priority: 1001\' | sudo tee /etc/apt/preferences.d/ensure.pref");
 			}
-			sh("wget -O - $p.ensure_repo_key | sudo apt-key add -")
-			sh("sudo apt-get update")
-			sh("sudo apt-get install --yes $pkg")
-			sh("sudo rm -f /etc/apt/sources.list.d/ensure.list /etc/apt/preferences.d/ensure.pref")
+			sh("wget -O - $p.ensure_repo_key | sudo apt-key add -");
+			sh("sudo apt-get update");
+			sh("sudo apt-get install --yes $pkg");
+			sh("sudo rm -f /etc/apt/sources.list.d/ensure.list /etc/apt/preferences.d/ensure.pref");
 		}
 		try {
-			check_pkgversion(pkg, dver)
+			check_pkgversion(pkg, dver);
 		} catch (err) {
-			logger.errorMsg("cannot install package: $pkg in desired version: $dver")
-			throw (err)
+			logger.errorMsg("cannot install package: $pkg in desired version: $dver");
+			throw (err);
 		}
 	}
 }
