eeec4938390c095d4f37065116a0dab0921014ab
Anna-Maria Gleixner <anna-maria@linutronix.de>
2018-02-26 13:57:55 +0100

    vars/shunit: Add try catch to catch exception messages
    When shunit command fails, the calling function has another context
    and the exception message get lost. Add a try/catch section in
    suitable context to get a reasonable error message.
    
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- vars/shunit.groovy
+++ vars/shunit.groovy
@@ -6,31 +6,38 @@
 import de.linutronix.lib4lib.logger;
 
 def call(String classname, String name, String cmd) {
+	try {
+		Map enspkg = [:]
+		enspkg.ensure_pkgs = [[name: 'pyjutest', version: '1.6+stretch251']];
+		// TODO skip suite name here and set it in ensureDebPkg
+		enspkg.ensure_repo = "deb http://debian.linutronix.de/tools stretch main"
+		enspkg.ensure_repo_key = "http://debian.linutronix.de/tools/repo.pub"
+		ensureDebPkg(enspkg);
 
-	Map enspkg = [:]
-	enspkg.ensure_pkgs = [[name: 'pyjutest', version: '1.6+stretch251']];
-	// TODO skip suite name here and set it in ensureDebPkg
-	enspkg.ensure_repo = "deb http://debian.linutronix.de/tools stretch main"
-	enspkg.ensure_repo_key = "http://debian.linutronix.de/tools/repo.pub"
-	ensureDebPkg(enspkg);
+		writeFile(file:"pyjutest.sh", text:cmd);
+		sh "chmod a+x ./pyjutest.sh"
 
-	writeFile(file:"pyjutest.sh", text:cmd);
-	sh "chmod a+x ./pyjutest.sh"
-
-	testcmd = "pyjutest";
-	if (classname) {
-		testcmd += " --class \"${classname}\"";
+		testcmd = "pyjutest";
+		if (classname) {
+			testcmd += " --class \"${classname}\"";
+		}
+		if (name) {
+			testcmd += " --name \"${name}\"";
+		}
+		testcmd += " ./pyjutest.sh";
+		logger.debugMsg("--- Start Test ---");
+		logger.debugMsg(cmd);
+		logger.debugMsg("run: " + testcmd);
+		sh(testcmd);
+		logger.debugMsg("--- End Test ---");
+		return junit_result("pyjutest.xml");
+	}  catch(Exception ex) {
+		println("shunit failed:");
+		println(ex.toString());
+		println(ex.getMessage());
+		println(ex.getStackTrace());
+		error("shunit failed.");
 	}
-	if (name) {
-		testcmd += " --name \"${name}\"";
-	}
-	testcmd += " ./pyjutest.sh";
-	logger.debugMsg("--- Start Test ---");
-	logger.debugMsg(cmd);
-	logger.debugMsg("run: " + testcmd);
-	sh(testcmd);
-	logger.debugMsg("--- End Test ---");
-	return junit_result("pyjutest.xml");
 }
 
 def call(String classname, String cmd) {
