metux/jenkins-cirt-libs

commit bbb02fe63848493c426213068df4902ce2f5dbb2

boottestRunner: Do not notify on cyclictest failures
If a cyclictest failed, currentBuild.result is set to UNSTABLE and
a user get notified for a failing boottest.

Recognize cyclictest failures and skip notification if needed.

Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

vars/boottestRunner.groovy
@@ -15,6 +15,12 @@ class BoottestException extends RuntimeException {
         }
 }
 
+class CyclictestException extends RuntimeException {
+	CyclictestException (String message) {
+		super(message);
+	}
+}
+
 private rebootTarget(String hypervisor, String target, String seriallog, Boolean testboot) {
 	def pidfile = "seriallogpid";
 	def virshoutput = "virshoutput";
@@ -194,6 +200,10 @@ private runner(Map global, helper helper, String boottest, String boottestdir, S
 				if (cyclictests) {
 					cyclictest(global, target, cyclictests,
 						   recipients);
+
+					if (currentBuild.result == 'UNSTABLE') {
+						throw new CyclictestException("One or more cyclictests failed.");
+					}
 				}
 			} finally {
 				println("Reboot into default kernel");
@@ -237,6 +247,7 @@ private failnotify(Map global, String repo, String branch, String config,
 
 def call(Map global, String boottest, String recipients) {
 	def failed = false;
+	def cyclictestFailed = false;
 	h = new helper();
 	try {
 		inputcheck.check(global);
@@ -269,6 +280,8 @@ def call(Map global, String boottest, String recipients) {
 		}
 	} catch (BoottestException ex) {
 		failed = true;
+	} catch (CyclictestException ex) {
+		cyclictestFailed = true;
 	} catch(Exception ex) {
 		println("boottest \"${boottest}\" failed:");
 		println(ex.toString());
@@ -309,9 +322,13 @@ def call(Map global, String boottest, String recipients) {
 		      includes: "${resultdir}/pyjutest.xml, " +
 		      "${resultdir}/cmdline");
 
-		if (currentBuild.result == 'UNSTABLE') {
-			failnotify(global, repo, branch, config, overlay,
-				   resultdir, recipients);
+		/* Do not notify on failed cyclictest nor stable test */
+		if (cyclictestFailed || currentBuild.result != 'UNSTABLE') {
+			return;
+		}
+
+		failnotify(global, repo, branch, config, overlay, resultdir,
+			   recipients);
 	}
 }
 

commit 095c3998d30e29052d2f3ce01ba59db838619504

boottestRunner: Remove CyclictestException
commit bbb02fe63848 ("boottestRunner: Do not notify on cyclictest
failures") introduced CyclictestException to avoid double notification
on cyclictest failures. There the condition for notification relied on
currentBuild.result value per stage. This was not working and replaced
by self defined variables to store the results.

CyclictestException is now redundant and not needed by error handling
any more. Remove CyclictestException from error handling in
boottestRunner.

[anna-maria: Update commit message]

Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

vars/boottestRunner.groovy
@@ -38,12 +38,6 @@ class ForcedRebootAndBoottestException extends RuntimeException {
         }
 }
 
-class CyclictestException extends RuntimeException {
-	CyclictestException (String message) {
-		super(message);
-	}
-}
-
 private rebootTarget(String hypervisor, String target, String seriallog,
 		     Boolean testboot, Boolean forced) {
 	if (testboot && forced) {
@@ -279,12 +273,8 @@ private runner(Map global, helper helper, String boottest, String boottestdir, S
 
 				def cyclictests = safesplit.split(helper.getVar("CYCLICTESTS", " "));
 				if (cyclictests) {
-					def cyclicresult = cyclictest(global, target, cyclictests,
-								      recipients);
-
-					if (cyclicresult == 'UNSTABLE') {
-						throw new CyclictestException("One or more cyclictests failed.");
-					}
+					cyclictest(global, target, cyclictests,
+						   recipients);
 				}
 
 				println("Not forced reboot into default kernel");
@@ -349,7 +339,6 @@ private failnotify(Map global, String subject, String template, String repo,
 
 def call(Map global, String boottest, String recipients) {
 	def failed = false;
-	def cyclictestFailed = false;
 	def boottestdir = "";
 	def resultdir = "boottest";
 	def repo = "";
@@ -406,8 +395,6 @@ def call(Map global, String boottest, String recipients) {
 		}
 	} catch (BoottestException ex) {
 		failed = true;
-	} catch (CyclictestException ex) {
-		cyclictestFailed = true;
 	} catch (TargetOnOfflineException ex) {
 		println("On/Offline problem with target: ${target}");
 
@@ -468,9 +455,8 @@ def call(Map global, String boottest, String recipients) {
 
 			junit("${resultdir}/pyjutest.xml");
 
-			/* Do not notify on failed cyclictest nor stable test */
-			if (cyclictestFailed || !failed) {
-			return;
+			if (!failed) {
+				return;
 			}
 
 			failnotify(global,