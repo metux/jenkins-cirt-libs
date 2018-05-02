metux/jenkins-cirt-libs

commit 7e58cbd382a43687287ede73f8999ceb31315190

boottestRunner: Add bootfailure notifications for boot and forced boot
Add notifications for failed boot and failed forced reboot.

Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

vars/boottestRunner.groovy
@@ -294,6 +294,11 @@ private runner(Map global, helper helper, String boottest, String boottestdir, S
 				forcedRebootDefault(hypervisor, target, seriallog_default, true);
 				throw ex;
 			} catch (RebootException ex) {
+				failnotify(global,
+					   "boottestRunner - Reboot of \"${target}\" failed",
+					   "boottestReboot", global, repo,
+					   branch, config, overlay, resultdir,
+					   recipients, target);
 				forcedRebootDefault(hypervisor, target, seriallog_default, false);
 			}
 		}
@@ -379,9 +384,25 @@ def call(Map global, String boottest, String recipients) {
 		}
 	} catch (ForcedRebootAndBoottestException ex) {
 		failed = true;
+		if (params.GUI_FAILURE_NOTIFICATION) {
+			failnotify(global,
+				   "Forced reboot of \"${target}\" failed",
+				   "boottestRunner", global, repo, branch,
+				   config, overlay, resultdir,
+				   "${params.GUI_FAILURE_NOTIFICATION}",
+				   target, ["bootexception": true]);
+		}
 	} catch (ForcedRebootException ex) {
 		/* do not fail on dut testkernel reboot failures */
 		failed = false;
+		if (params.GUI_FAILURE_NOTIFICATION) {
+			failnotify(global,
+				   "Forced reboot of \"${target}\" failed",
+				   "boottestRunner", global, repo, branch,
+				   config, overlay, resultdir,
+				   "${params.GUI_FAILURE_NOTIFICATION}",
+				   target, ["bootexception": false]);
+		}
 	} catch (BoottestException ex) {
 		failed = true;
 	} catch (CyclictestException ex) {

commit 5bc55ab9cae967b7d5fc21b1e8cee27fb3c009eb

boottestRunner: Fix MissingPropertyException
For email notification after RebootException 'repo' needs to be
available and set. In function runner(), where the exception is raised
and handled, 'repo' is not available and MissingPropertyException is
raised.

Add and set the required parameter to be available for email
notification call.

Fixes: 7e58cbd382a4 ("boottestRunner: Add bootfailure notifications
       for boot and forced boot")
Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>
Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

vars/boottestRunner.groovy
@@ -294,6 +294,7 @@ private runner(Map global, helper helper, String boottest, String boottestdir, S
 				forcedRebootDefault(hypervisor, target, seriallog_default, true);
 				throw ex;
 			} catch (RebootException ex) {
+				def repo = helper.getVar("GITREPO");
 				failnotify(global,
 					   "boottestRunner - Reboot of \"${target}\" failed",
 					   "boottestReboot", global, repo,